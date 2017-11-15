a3517957100630a6c119232c9feffd54d8ebf1e2
fbarriosr <francisco.barriosr@gmail.com>
2017-11-14 22:00:18 -0300

    primer lanzamiento

---
+++ EcmoScan/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EcmoScan
+//
+//  Created by Francisco Barrios Romo on 18-09-17.
+//  Copyright © 2017 RentalApps. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
